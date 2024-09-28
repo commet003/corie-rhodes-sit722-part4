78ef6d76e311ec75ca709b7715f1c151a8b264a6
Corie Rhodes <corierhodes@yahoo.com.au>
2024-09-28 16:08:14 +1000

    Config completed, all working.

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://part04_user:xuxzkAQukYtrPGuuCRwOTjBnE07ZWSnE@dpg-crrmskt6l47c73clfl1g-a.oregon-postgres.render.com/part04')
